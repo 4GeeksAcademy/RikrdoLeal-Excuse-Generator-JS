ff246ec069f9f71032bb6edac4c7fa4eecd36b11
RikrdoLeal <140850677+RikrdoLeal@users.noreply.github.com>
2024-02-24 14:09:08 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The", "My"];
+  let subject = ["cat", "dog", "wife", "joker", "politician"];
+  let action = ["took my", "threw my", "stole my", "broke my", "ate my"];
+  let possession = ["homework", "money", "car", "pants"];
+  let where = [
+    "on the street",
+    "in my house",
+    "at the supermarket",
+    "at the party"
+  ];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possession[possessionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
